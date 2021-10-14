30f5f43d1b21f5f7bcd809af3814e27a23e0ee6e
Lucas SPITERI <lucasspiteri@protonmail.com>
2021-10-14 09:13:28 +0200

    Rajout d'une implémentation de fibo avec une boucle for

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 /*
 * Programme à tester sur la carte
-* TODO: Faire une fonction fibonaccci
+* TODO: Absolument besoin de faire une version récursive ?
 */
 
 #include <stdint.h>
+
+uint32_t a;
+uint32_t b;
+uint32_t c;
+
+uint32_t fibo(uint32_t n) {
+	uint32_t out = 0;
+	if(n < 2)
+		return n;
+	
+	for(uint32_t i = 0; i < n - 1; i++) {
+		out += i;
+	}
+
+	return out;
+}
+
+int main() {
+	uint32_t o = fibo(8);
+	return o;
+}
