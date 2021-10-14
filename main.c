160992beba081fedc44e26928c0797d4abb42112
Lucas SPITERI <53694411+Thunor12@users.noreply.github.com>
2021-10-14 10:38:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,24 +5,6 @@
 
 #include <stdint.h>
 
-// Variables non initialisée pour tester la BSS
-uint32_t a;
-uint32_t b;
-uint32_t c;
-
-uint32_t fibo(uint32_t n) {
-	uint32_t out = 0;
-	if(n < 2)
-		return n;
-	
-	for(uint32_t i = 0; i < n - 1; i++) {
-		out += i;
-	}
-
-	return out;
-}
-
 int main() {
-	uint32_t o = fibo(8);
 	return o;
 }
