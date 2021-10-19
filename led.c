5b5d8aa11cfec059a57fad40ea1103a1f1c4d6b1
Mourad <mourad.bouabdallah@telecom-paris.fr>
2021-10-19 22:46:29 +0200

    Modifier quelque registre

--- led.c
+++ led.c
@@ -2,36 +2,29 @@
 #include "led.h"
 #include "stm32l475xx.h"
 
-/*#define RCC_BASE 0x40021000
-#define RCC_AHB2ENR *((volatile uint32_t*)(RCC_BASE+0x4C))
-
-#define GPIOB_BASE 0x48000400 
-#define GPIOC_BASE 0x48000800 
-#define GPIOB_MODER *((volatile uint32_t*)(GPIOB_BASE+0x00))
-#define GPIOC_MODER *((volatile uint32_t*)(GPIOC_BASE+0x00))
-#define GPIOB_ODR   *((volatile uint32_t*)(GPIOB_BASE+0x14))
-#define GPIOC_BSRR   *((volatile uint32_t*)(GPIOC_BASE+0x18))*/
-
 
 
 
 void led(enum state state)
 {
-   if(state ==0)
+   if(state == LED_OFF)
    {
-    GPIOC->MODER &= ~ GPIO_MODER_MODE9;//GPIOC_MODER =(GPIOC_MODER & ~(0x3<<18U)); //PC9 est en entrée (= haute impédance), les deux LED sont éteintes.
+      // configurer en entrée
+    GPIOC->MODER &= ~ GPIO_MODER_MODE9;//PC9 est en entrée (= haute impédance), les deux LED sont éteintes.
     
    }
-   else if (state == 1)
+   else if (state == LED_YELLOW)
    {
-
-     GPIOC->MODER |= GPIO_MODER_MODE9_0; //GPIOC_MODER =(GPIOC_MODER & ~(0x3<<18U))| (0x1<<18U);
-    GPIOC->BRR |= GPIO_BRR_BR_9; // GPIOC_BSRR  =(GPIOC_BSRR  & ~(0x1<<9U))| (0x1<<9U);  //PC9 est en sortie à l'état haut, LED3 est allumée, LED4 est éteinte.
+      // sortie, état haut
+      GPIOC->BSRR = GPIO_BSRR_BS9; //PC9 est en sortie à l'état haut, LED3 est allumée, LED4 est éteinte.
+      GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODE9_Msk) | GPIO_MODER_MODE9_0; 
    }
    else
    {
-     GPIOC->MODER |= GPIO_MODER_MODE9_0;// GPIOC_MODER =(GPIOC_MODER & ~(0x3<<18U))| (0x1<<18U);
-     GPIOC->BSRR |= GPIO_BSRR_BR_9; //GPIOC_BSRR  =(GPIOC_BSRR  & ~(0x1<<9U))| (0x1<<25U);  //PC9 est en sortie à l'état bas, LED3 est éteinte, LED4 est allumée.
+      // sorti, état bas 
+     GPIOC->BSRR   = GPIO_BSRR_BR9;  //PC9 est en sortie à l'état bas, LED3 est éteinte, LED4 est allumée.
+     GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODE9_Msk) | GPIO_MODER_MODE9_0;
+     //GPIOC->BRR = GPIO_BRR_BR9;
    }
 }
 
@@ -39,18 +32,17 @@ void led_init()
 {
    //activation du clock du GPIOB et GPIOC
    
-  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOCEN;//RCC_AHB2ENR =(RCC_AHB2ENR & ~(0x1<<1U))| (0x1<<1U);  //IO port B clock enabled
-                                                            //RCC_AHB2ENR =(RCC_AHB2ENR & ~(0x1<<2U))| (0x1<<2U);  //IO port C clock enabled
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOCEN);//IO port B clock enabled et IO port C clock enabled
+                                                            
    //PB14 en mode sortie
-   GPIOB->MODER =0;//init MODER
-   GPIOB->MODER |= GPIO_MODER_MODE14_0;//GPIOB_MODER =(GPIOB_MODER & ~(0x3<<28U))| (0x1<<28U);
+   GPIOB->MODER = (GPIOB->MODER & ~ GPIO_MODER_MODE14_Msk) | GPIO_MODER_MODE14_0;
    
 }
 void led_g_on()
 {
-  GPIOB->ODR |= GPIO_ODR_OD14;//GPIOB_ODR  =(GPIOB_ODR & ~(0x1<<14U))| (0x1<<14U);//PB14 is on
+  GPIOB->ODR |= GPIO_ODR_OD14;//PB14 is on
 }
 void led_g_off()
 {
-   GPIOB->ODR &= ~GPIO_ODR_OD14;//GPIOB_ODR  =(GPIOB_ODR & ~(0x1<<14U));//PB14 is off
+   GPIOB->ODR &= ~GPIO_ODR_OD14;//PB14 is off
 }
